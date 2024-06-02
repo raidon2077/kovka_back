992df79d91c7c9544cca4abcb91096a8590ef9ca
raidon2077 <157355011+raidon2077@users.noreply.github.com>
2024-06-02 19:38:00 +0300

    Add files via upload

---
+++ script.sql
@@ -0,0 +1,14 @@
+-- Database: kovka
+
+-- DROP DATABASE IF EXISTS kovka;
+
+CREATE DATABASE kovka
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Russian_Russia.1251'
+    LC_CTYPE = 'Russian_Russia.1251'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
